d26420701907937f2685dadd8a34ac83160e97b6
Christian Oder <myself5@carbonrom.org>
2021-09-26 03:17:24 +0200

    doze: Adjust packagename for Carbon
    Change-Id: Ia6164d502ccb9f36e64f4b4daa0bb21fcdfaa87b

--- doze/src/org/carbonrom/settings/doze/DozeService.kt
+++ doze/src/org/carbonrom/settings/doze/DozeService.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.lineageos.settings.doze
+package org.carbonrom.settings.doze
 
 import android.app.Service
 import android.content.BroadcastReceiver
